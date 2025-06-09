2dfc60c2c7a2bd058d4886fff77b5914b807c2d8
MatheusSetmaier <matheussetmaier590@gmail.com>
2025-06-09 12:19:05 -0300

    .

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, redirect
 from db import get_connection
 
+
 app = Flask(__name__)
 
 @app.route('/')
--- db.py
+++ db.py
@@ -4,7 +4,8 @@ import mysql.connector
 def get_connection():
     
     return mysql.connector.connect(
-        host='localhost',
+        host='teste.hiperconnect.me',
+        port=8500,
         user='master',
         password='Teste@Master',
         database='banco_matheus'
